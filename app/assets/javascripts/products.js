40aa4e9c5375986239f3eeb7ad16bd026a263232
Stephen Wentling <swentling87@gmail.com>
2016-06-05 00:10:07 -0700

    further js correction for heroku

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,13 +1,1 @@
-var ready;
 
-ready = function() {
-  $(".main-header").hide().fadeIn(2500)
-  $(".side-bar-link").hover(function() {
-    $(this).toggleClass("side-bar-link-big")});
-  $(".thumbnail").hover(function() {
-    $(this).toggleClass("thumbnail-shadow")
-  })
-};
-
-$(document).ready(ready);
-$(document).on('page:load', ready);
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,21 +2,29 @@ var ready;
 
 ready = function() {
   $(".main-header").hide().fadeIn(2500)
-  $(".side-bar-link").hover(function() {
-    $(this).toggleClass("side-bar-link-big")});
+
+  $(".side-bar-link").mouseenter(function() {
+    $(this).addClass("side-bar-link-big")});
+  $(".side-bar-link").mouseleave(function() {
+    $(this).removeClass("side-bar-link-big")});
+
   $(".tabs").mouseenter(function() {
     $(this).addClass("tabs-enter")});
   $(".tabs").mouseleave(function() {
     $(this).removeClass("tabs-enter")});
+
   $(".links").mouseenter(function() {
     $(this).addClass("links-big")});
   $(".links").mouseleave(function() {
     $(this).removeClass("links-big")});
+
   $(document).ready(function() {
     $(".about").hide().slideDown(1000)});
-  $(".thumbnail").hover(function() {
-    $(this).toggleClass("thumbnail-shadow")
-  })
+
+  $(".thumbnail").mouseenter(function() {
+    $(this).addClass("thumbnail-shadow")});
+  $(".thumbnail").mouseleave(function() {
+    $(this).removeClass("thumbnail-shadow")});
 };
 
 $(document).ready(ready);
